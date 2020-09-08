968a8d6970b0df61598f3e46aae4e694090a0714
calloway <calloway@adobe.com>
2020-09-08 11:14:55 -0400

    Improving test cases.

--- src/main/java/plc/interpreter/Regex.java
+++ src/main/java/plc/interpreter/Regex.java
@@ -9,10 +9,10 @@ import java.util.regex.Pattern;
 public class Regex {
     public static final Pattern
             EMAIL = Pattern.compile("[A-Za-z0-9._-]+@[A-Za-z0-9-]*\\.[a-z]{2,3}"),
-            FILE_NAMES = Pattern.compile("(?<name>[\\w-]+)+[.a-z]*(.java|.class)"),
+            FILE_NAMES = Pattern.compile("(?<name>[\\w-]+)+([.][a-z]+)*(\\.java|\\.class)"),
             EVEN_STRINGS = Pattern.compile("([\\S]{10}|[\\S]{12}|[\\S]{14}|[\\S]{16}|[\\S]{18}|[\\S]{20})"),  //FIXME: very wrong lol
             INTEGER_LIST = Pattern.compile("([\\[])(([1-9]+[0-9]*)+(, {0,1}[1-9]+[0-9]*)*)*([\\]])"),
-            IDENTIFIER = Pattern.compile("(([.][.\\w\\+\\-\\*\\/\\:!\\?<>=]+)*([A-Za-z_\\+\\-\\*\\/:!\\?<>=]+[\\d.]*)*)"),  //FIXME: empty string check
+            IDENTIFIER = Pattern.compile("(([.][.\\w\\+\\-\\*\\/\\:!\\?<>=]+)*([A-Za-z_\\+\\-\\*\\/:!\\?<>=]+[\\d.]*)*)"),  //FIXME: empty string check?
             NUMBER = Pattern.compile("([\\+]|[\\-]){0,1}([\\d]+)+([.][\\d]+)*"),
             STRING = Pattern.compile("(\")([\\w,!\\-\\.+*/=]*([\\\\][bnrt'\"\\\\])*[\\w,!\\-\\.+*/=]*)*(\")");
 
