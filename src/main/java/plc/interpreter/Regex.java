329d7b07135150f1963c60dae6d6ff34c66ba049
calloway <calloway@adobe.com>
2020-09-07 21:35:43 -0400

    85% done, almost there!  Nothing to do until office hours.

--- src/main/java/plc/interpreter/Regex.java
+++ src/main/java/plc/interpreter/Regex.java
@@ -7,13 +7,12 @@ import java.util.regex.Pattern;
  * See the assignment page for resources on regex's as needed.
  */
 public class Regex {
-    //FIXME: Review individual expressions to make sure everything is grabbed properly
     public static final Pattern
             EMAIL = Pattern.compile("[A-Za-z0-9._-]+@[A-Za-z0-9-]*\\.[a-z]{2,3}"),
             FILE_NAMES = Pattern.compile("(?<name>[\\w-]+)+[.a-z]*(.java|.class)"),
             EVEN_STRINGS = Pattern.compile("([\\S]{10}|[\\S]{12}|[\\S]{14}|[\\S]{16}|[\\S]{18}|[\\S]{20})"),  //FIXME: very wrong lol
             INTEGER_LIST = Pattern.compile("([\\[])(([1-9]+[0-9]*)+(, {0,1}[1-9]+[0-9]*)*)*([\\]])"),
-            IDENTIFIER = Pattern.compile("(([.][.\\w\\+\\-\\*\\/\\:!\\?<>=]+)*([A-Za-z_\\+\\-\\*\\/:!\\?<>=]+[\\d.]*)*)"),
+            IDENTIFIER = Pattern.compile("(([.][.\\w\\+\\-\\*\\/\\:!\\?<>=]+)*([A-Za-z_\\+\\-\\*\\/:!\\?<>=]+[\\d.]*)*)"),  //FIXME: empty string check
             NUMBER = Pattern.compile("([\\+]|[\\-]){0,1}([\\d]+)+([.][\\d]+)*"),
             STRING = Pattern.compile("(\")([\\w,!\\-\\.+*/=]*([\\\\][bnrt'\"\\\\])*[\\w,!\\-\\.+*/=]*)*(\")");
 
