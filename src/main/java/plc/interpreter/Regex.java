3f882e12e47928a9592e5f5a073bf891e134f318
calloway <calloway@adobe.com>
2020-09-10 21:51:13 -0400

    DONE AGAIN.

--- src/main/java/plc/interpreter/Regex.java
+++ src/main/java/plc/interpreter/Regex.java
@@ -13,7 +13,7 @@ public class Regex {
             EVEN_STRINGS = Pattern.compile("(([\\S\\s]){20}|([\\S\\s]){18}|([\\S\\s]){16}|([\\S\\s]){14}|([\\S\\s]){12}|([\\S\\s]){10})"),
             INTEGER_LIST = Pattern.compile("([\\[])(([1-9]+[0-9]*)+(, {0,1}[1-9]+[0-9]*)*)*([\\]])"),
             IDENTIFIER = Pattern.compile("[A-Za-z_+\\-*/.:!?<>=]{2,}|([A-Za-z_+\\-*/:!?<>=]+[\\w._+\\-*/:!?<>=]*)|[.][\\w+\\-*/:!?<>=]+"),
-            NUMBER = Pattern.compile("([\\+]|[\\-]){0,1}([\\d]+)+([.][\\d]+)*"),
-            STRING = Pattern.compile("(\"[\\w\\s,!\\-\\.+*/=]*([\\\\][bnrt'\"\\\\]{1})*[\\w\\s,!\\-\\.+*/=]*\")+");
+            NUMBER = Pattern.compile("([\\+]|[\\-]){0,1}[\\d]+([.][\\d]+)*"),
+            STRING = Pattern.compile("(\")[^\\\\]*(\\\\[bnrt'\"\\\\])*[^\\\\]*(\")");
 
 }
